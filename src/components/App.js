a197d83440226599e276c84e1a2c6d32d8f400e4
ichirota nakada <ichirota@sakinoMacBook-Pro.local>
2020-02-06 21:00:44 +0900

    implement component with function

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
+import { connct } from 'react-redux';
 
-const App = () => (<Counter></Counter>)
+import { increment, decrement } from '../actions';
 
 class Counter extends Component{
-  constructor(props) {
-    super(props)
-    console.log(this.state)
-    this.state = { count: 0 }
-  }
-
-  handlePlusButoon = () => {
-      this.setState({ count: this.state.count + 1})
-  }
-
-  handleMinusButoon = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-
   render() {
+    const props = this.props
     return (
     <React.Fragment>
-      <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButoon}>+1</button>
-        <button onClick={this.handleMinusButoon}>-1</button>
+      <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
     </React.Fragment >
       )
     }
-  }
+}
   
-  export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+const mapDispatchToProps = dispatch => ({
+  increment: () => (increment()),
+  decrement: () => (decrement())
+})
+
+// const mapDispatchToProps = ({ increment, decrement })
+  
+export default connct(mapStateToProps, mapDispatchToProps)(App)
