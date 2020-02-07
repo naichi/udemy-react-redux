0d4cef7b67554cb4f2988690666b2be55238f957
ichirota nakada <ichirota@sakinoMacBook-Pro.local>
2020-02-07 17:05:27 +0900

    impolement event new component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
-        < EventsIndex />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/events/new" component={EventsNew} />
+                <Route exact path="/" component={EventsIndex} />
+            </Switch>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
