97a8a322b5c2f759a6985e5702b9c6d31d5a85a1
ichirota nakada <ichirota@sakinoMacBook-Pro.local>
2020-02-07 18:05:54 +0900

    insert submit info

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 
+// このへんん何してるかよくわからん
+const enhancer = process.env.NODE_ENV === 'development' ?
+    composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
