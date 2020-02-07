c9cf72c9deddc1c68e066b0eff41a710505faab5
ichirota nakada <ichirota@sakinoMacBook-Pro.local>
2020-02-07 17:49:46 +0900

    inplement new component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+// ここでformを'redux-form'からimportしていないと、formが機能しないので文字入力などができないっぽい
+export default combineReducers({ events, form })
